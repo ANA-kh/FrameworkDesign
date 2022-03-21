1659cc80b3fc621913fa0735c51501ceb1056608
unknown <weikang.huang@cn.corp.seagroup.com>
2022-03-21 15:54:06 +0800

    create project

---
+++ Assets/FrameworkDesign/Example/Script/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace FrameworkDesign.Example
+{
+    public class Enemy : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public GameObject GamePassPanel;
+        private static int mKilledEnemyCount = 0; 
+        private void OnMouseDown()
+        {
+            Destroy(gameObject);
+
+            mKilledEnemyCount++;
+
+            if (mKilledEnemyCount == 4)
+            {
+                GamePassPanel.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/FrameworkDesign/Example/Script/UI/GameStartPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FrameworkDesign.Example
+{
+    public class GameStartPanel : MonoBehaviour
+    {
+        public GameObject Enemies;
+        void Start()
+        {
+            transform.Find("BtnStart").GetComponent<Button>().onClick.AddListener(() =>
+            {
+                gameObject.SetActive(false);
+                Enemies.SetActive(true);
+            });
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
