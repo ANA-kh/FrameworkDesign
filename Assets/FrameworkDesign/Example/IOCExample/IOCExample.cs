bc6f0e2310b66944d5688256d956157093528873
ANA <1154073432@qq.com>
2022-03-24 17:04:11 +0800

    simple IOC example

---
+++ Assets/FrameworkDesign/Example/IOCExample/IOCExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace FrameworkDesign.Example
+{
+    public class IOCExample : MonoBehaviour
+    {
+        private void Start()
+        {
+            var container = new IOCContainer();
+            
+            container.Register(new BluetoothManager());
+
+            var bluetoorhManager = container.Get<BluetoothManager>();
+            
+            bluetoorhManager.Connect();
+        }
+    }
+
+    public class BluetoothManager
+    {
+        public void Connect()
+        {
+            Debug.Log("蓝牙连接成功!");
+        }
+    }
+}
---
+++ Assets/FrameworkDesign/Framework/IOC/IOCContainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FrameworkDesign
+{
+    public class IOCContainer
+    {
+        Dictionary<Type,object> instances = new Dictionary<Type, object>();
+
+        public void Register<T>(T instance)
+        {
+            var key = typeof(T);
+
+            if (instances.ContainsKey(key))
+            {
+                instances[key] = instance;
+            }
+            else
+            {
+                instances.Add(key,instance);
+            }
+        }
+
+        public T Get<T>() where T : class
+        {
+            var key = typeof(T);
+            if (instances.TryGetValue(key, out var retInstance))
+            {
+                return retInstance as T;
+            }
+            
+            return null;
+        }
+    }
+}
