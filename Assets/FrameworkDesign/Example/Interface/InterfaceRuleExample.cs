87167a2cd699b6fe0be910de9d9b238d39fe1b8e
ANA_H <1154073432@qq.com>
2022-04-04 15:48:37 +0800

    explicit interface useage

---
+++ Assets/FrameworkDesign/Example/Interface/InterfaceRuleExample.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using UnityEngine;
+
+namespace FrameworkDesign.Example
+{
+    public class CanDoEverything
+    {
+        public void DoSomething1()
+        {
+            Debug.Log("DoSomething1");
+        }
+
+        public void DoSomething2()
+        {
+            Debug.Log("DoSomething2");
+        }
+
+        public void DoSomething3()
+        {
+            Debug.Log("DoSomething3");
+        }
+    }
+
+    public interface IHasEverything
+    {
+        CanDoEverything CanDoEverything { get;}
+    }
+
+    public interface ICanDoSomething1 : IHasEverything
+    {
+        
+    }
+
+    public static class ICanDoSomething1Extension
+    {
+        public static void DoSomething1(this ICanDoSomething1 selt)
+        {
+            selt.CanDoEverything.DoSomething1();
+        }
+    }
+    public interface ICanDoSomething2 : IHasEverything
+    {
+        
+    }
+
+    public static class ICanDoSomething2Extension
+    {
+        public static void DoSomething2(this ICanDoSomething2 selt)
+        {
+            selt.CanDoEverything.DoSomething2();
+        }
+    }
+    public interface ICanDoSomething3 : IHasEverything
+    {
+        
+    }
+
+    public static class ICanDoSomething3Extension
+    {
+        public static void DoSomething3(this ICanDoSomething3 selt)
+        {
+            selt.CanDoEverything.DoSomething3();
+        }
+    }
+    
+    
+    
+    
+    public class InterfaceRuleExample : MonoBehaviour
+    {
+        public class OnlyCanDo1 : ICanDoSomething1
+        {
+            CanDoEverything IHasEverything.CanDoEverything { get; } = new CanDoEverything();
+        }
+        
+        public class OnlyCanDo23 : ICanDoSomething2,ICanDoSomething3
+        {
+            CanDoEverything IHasEverything.CanDoEverything { get; } = new CanDoEverything();
+        }
+
+        private void Start()
+        {
+            var onlyCanDo1 = new OnlyCanDo1();
+            //onlyCanDo1.CanDoEverything.DoSomething1(); 报错，必须转换为IHasEverything去调用；  实现了
+            onlyCanDo1.DoSomething1();
+            
+            var onlyCanDo23 = new OnlyCanDo23();
+            onlyCanDo23.DoSomething2();
+            onlyCanDo23.DoSomething3();
+        }
+    }
+}
---
+++ Assets/FrameworkDesign/Example/Interface/InterfaceStructExample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+namespace FrameworkDesign.Example
+{
+    public class InterfaceStructExample : MonoBehaviour
+    {
+        public interface ICustomScript
+        {
+            void Start();
+            void Update();
+            void Destroy();
+        }
+        
+        public abstract class CustomScript : ICustomScript
+        {
+            void ICustomScript.Start()
+            {
+                OnStart();
+            }
+
+            void ICustomScript.Update()
+            {
+                OnUpdate();
+            }
+
+            void ICustomScript.Destroy()
+            {
+                OnDestroy();
+            }
+
+            public abstract void OnStart();
+            public abstract void OnUpdate();
+            public abstract void OnDestroy();
+        }
+        
+        public class MyScript : CustomScript
+        {
+            public override void OnStart()
+            {
+                //Start();会造成死循环   使用explicit interface使得Start()无法直接调用
+                Debug.Log("OnStart");
+            }
+
+            public override void OnUpdate()
+            {
+                Debug.Log("OnUpdate");
+            }
+
+            public override void OnDestroy()
+            {
+                Debug.Log("OnDestroy");
+            }
+        }
+
+        private void Start()
+        {
+            ICustomScript myScript = new MyScript();
+            myScript.Start();
+            myScript.Update();
+            myScript.Destroy();
+        }
+    }
+}
